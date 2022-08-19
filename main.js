6848534c442c87e0ad8d48efc407154391c89b60
Samaneh Farhadi <ifarseen25@gmail.com>
2022-08-20 01:15:34 +0430

    javascript file uploaded

---
+++ main.js
@@ -0,0 +1,26 @@
+function showTime(){
+    let date = new Date();
+    let h = date.getHours();
+    let m = date.getMinutes();
+    let s = date.getSeconds();
+    let session = "AM";
+
+    if (h === 0) {
+        h = 12;
+    }
+
+    if (h > 12) {
+        h = h - 12;
+        session = "PM";
+    }
+
+    h = (h < 10 ) ? `0${h}`: h;
+    m = (m < 10 ) ? `0${m}`: m;
+    s = (s < 10 ) ? `0${s}`: s;
+
+    let time = `${h}:${m}:${s} ${session}`;
+    document.querySelector(".clock").innerHTML = time;
+
+}
+
+setInterval(showTime, 1000);
